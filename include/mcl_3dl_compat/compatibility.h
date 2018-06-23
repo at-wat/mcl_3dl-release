afbcdd55d20568a32c5c27992572ac802ce7732f
Atsushi Watanabe <atsushi.w@ieee.org>
2018-06-23 09:18:40 +0000

    Rebase from 'upstream'

--- include/mcl_3dl_compat/compatibility.h
+++ include/mcl_3dl_compat/compatibility.h
@@ -27,8 +27,8 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef COMPATIBILITY_H
-#define COMPATIBILITY_H
+#ifndef MCL_3DL_COMPAT_COMPATIBILITY_H
+#define MCL_3DL_COMPAT_COMPATIBILITY_H
 
 #include <ros/ros.h>
 
@@ -80,10 +80,10 @@ void checkCompatMode()
     ROS_ERROR_ONCE(
         "======= [Deprecated] %s is run in compatible mode =======\n"
         "=========================================================\n"
-        "Set _compatible:=false to switch to new topic namespaces.\n"
+        "Set _compatible:=%d to switch to new topic namespaces.\n"
         "Compatible mode will be obsolated in the future update.\n"
         "=========================================================",
-        ros::this_node::getName().c_str());
+        ros::this_node::getName().c_str(), current_level);
   }
 }
 std::string getSimplifiedNamespace(ros::NodeHandle &nh)
@@ -196,4 +196,4 @@ ros::ServiceServer advertiseService(
 }
 }  // namespace mcl_3dl_compat
 
-#endif  // COMPATIBILITY_H
+#endif  // MCL_3DL_COMPAT_COMPATIBILITY_H
