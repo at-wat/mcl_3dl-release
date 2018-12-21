4ddc1f136ab11d13280ec5a8e653818ad36fadee
Atsushi Watanabe <atsushi.w@ieee.org>
2018-12-21 07:01:39 +0000

    Rebase from 'debian/melodic/mcl_3dl'

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
 set -o errexit
-set -o verbose
 
-pip install gh-pr-comment
+pip install gh-pr-comment catkin_lint
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
+set -o verbose
+
 cd /catkin_ws
+
+
+pkgs=`find . -name package.xml | xargs -n1 dirname`
+catkin_lint $pkgs \
+  || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" \
+      "<details><summary>catkin_lint failed</summary>
+
+\`\`\`
+`catkin_lint $pkgs 2>&1`
+\`\`\`
+</details>"; false)
 
 mkdir -p /catkin_ws/build/mcl_3dl/test/
 mv /catkin_ws/src/mcl_3dl/.cached-dataset/* /catkin_ws/build/mcl_3dl/test/
 ls -lh /catkin_ws/build/mcl_3dl/test/
 
-sed -i -e '5a set(CMAKE_C_FLAGS "-Wall -Werror")' \
+sed -i -e '5a set(CMAKE_C_FLAGS "-Wall -Werror -O1 -coverage")' \
   /opt/ros/${ROS_DISTRO}/share/catkin/cmake/toplevel.cmake
-sed -i -e '5a set(CMAKE_CXX_FLAGS "-Wall -Werror")' \
+sed -i -e '5a set(CMAKE_CXX_FLAGS "-Wall -Werror -O1 -coverage")' \
   /opt/ros/${ROS_DISTRO}/share/catkin/cmake/toplevel.cmake
 
-CM_OPTIONS=""
-if [ x${ROS_DISTRO} == "xindigo" ]
-then
-  CM_OPTIONS="${CM_OPTIONS} -DCMAKE_BUILD_TYPE=Release"
-  echo "On indigo-trusty, we need release build due to the bug of PCL1.7 with c++11." 1>&2
-fi
+CM_OPTIONS=''
 
-catkin_make ${CM_OPTIONS} || \
-  (gh-pr-comment "FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
+catkin_make -DMCL_3DL_EXTRA_TESTS=ON ${CM_OPTIONS} || \
+  (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make``` failed'; false)
 catkin_make tests -DMCL_3DL_EXTRA_TESTS=ON ${CM_OPTIONS} || \
-  (gh-pr-comment "FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
+  (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make tests``` failed'; false)
 catkin_make run_tests -DMCL_3DL_EXTRA_TESTS=ON ${CM_OPTIONS} || \
-  (gh-pr-comment "FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
+  (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" '```catkin_make run_tests``` failed'; false)
 
 if [ catkin_test_results ];
 then
@@ -47,10 +54,12 @@ else
 `find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;'`
 "
 fi
-catkin_test_results || (gh-pr-comment "FAILED on ${ROS_DISTRO}" "Test failed$result_text"; false)
+catkin_test_results || (gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] FAILED on ${ROS_DISTRO}" "<details><summary>Test failed</summary>
 
-gh-pr-comment "PASSED on ${ROS_DISTRO}" "All tests passed$result_text" || true
+$result_text</details>"; false)
 
-cd ..
-rm -rf /catkin_ws || true
+(cd src/mcl_3dl/; cp -r /catkin_ws/build ./; bash <(curl -s https://codecov.io/bash) -y .codecov.yml)
 
+gh-pr-comment "[#${TRAVIS_BUILD_NUMBER}] PASSED on ${ROS_DISTRO}" "<details><summary>All tests passed</summary>
+
+$result_text</details>" || true
