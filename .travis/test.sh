1c322193b7abeb30722e09822edfc164895b9237
Atsushi <atsushi.w@ieee.org>
2020-09-07 06:08:44 +0000

    Rebase from 'debian/noetic/mcl_3dl'

--- .travis/test.sh
+++ .travis/test.sh
@@ -5,24 +5,30 @@ set -o errexit
 source /opt/ros/${ROS_DISTRO}/setup.bash
 cd /catkin_ws
 
+md_codeblock='```'
+
 pkgs=$(find . -name package.xml | xargs -n1 dirname)
 catkin_lint $pkgs \
   || (gh-pr-comment "${BUILD_LINK} FAILED on ${ROS_DISTRO}" \
       "<details><summary>catkin_lint failed</summary>
 
-\`\`\`
+${md_codeblock}
 $(catkin_lint $pkgs 2>&1)
-\`\`\`
+${md_codeblock}
 </details>"; false)
 
 mkdir -p /catkin_ws/build/mcl_3dl/test/
 mv /catkin_ws/src/mcl_3dl/.cached-dataset/* /catkin_ws/build/mcl_3dl/test/
 ls -lh /catkin_ws/build/mcl_3dl/test/
 
-sed -i -e '5a set(CMAKE_C_FLAGS "-Wall -Werror -O1 -coverage")' \
+sed -i -e "/^set(CATKIN_TOPLEVEL TRUE)$/a set(CMAKE_C_FLAGS \"-Wall -Werror -O2 -coverage\")" \
   /opt/ros/${ROS_DISTRO}/share/catkin/cmake/toplevel.cmake
-sed -i -e '5a set(CMAKE_CXX_FLAGS "-Wall -Werror -O1 -coverage")' \
+sed -i -e "/^set(CATKIN_TOPLEVEL TRUE)$/a set(CMAKE_CXX_FLAGS \"-Wall -Werror -O2 -coverage\")" \
   /opt/ros/${ROS_DISTRO}/share/catkin/cmake/toplevel.cmake
+
+echo "--- catkin cmake hook ---"
+grep -A5 -B1 "set(CATKIN_TOPLEVEL TRUE)" /opt/ros/${ROS_DISTRO}/share/catkin/cmake/toplevel.cmake
+echo "-------------------------"
 
 CM_OPTIONS=''
 
@@ -36,15 +42,15 @@ catkin_make run_tests -DMCL_3DL_EXTRA_TESTS=ON ${CM_OPTIONS} || \
 if [ catkin_test_results ];
 then
   result_text="
-\`\`\`
+${md_codeblock}
 $(catkin_test_results --all | grep -v Skipping || true)
-\`\`\`
+${md_codeblock}
 "
 else
   result_text="
-\`\`\`
+${md_codeblock}
 $(catkin_test_results --all | grep -v Skipping || true)
-\`\`\`
+${md_codeblock}
 $(find build/test_results/ -name *.xml | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`; xmllint --format $0; echo \\\`\\\`\\\`;')
 "
 fi
@@ -52,9 +58,41 @@ catkin_test_results || (gh-pr-comment "${BUILD_LINK} FAILED on ${ROS_DISTRO}" "<
 
 $result_text</details>"; false)
 
-(cd src/mcl_3dl/;
-  cp -r /catkin_ws/build ./;
-  bash <(curl -s https://codecov.io/bash) -y .codecov.yml -X gcovout)
+if [ x${COVERAGE_TEST:-true} == "xtrue" ]
+then
+  set -o pipefail
+
+  echo
+  echo "Generated gcda files"
+  find /catkin_ws/build -name "*.gcda" | xargs -n1 echo "  -"
+
+  # Find and copy renamed gcda files
+  echo
+  echo "Renamed gcda files"
+  find /tmp/gcov/ -name "*.gcda" | sed 's|^/tmp/gcov||' | while read file
+  do
+    id=$(echo ${file} | cut -d'/' -f2) # /id/path/to/gcda
+    gcda=$(echo ${file} | sed 's|^/[^/]*/|/|')
+    new_gcda=$(echo ${gcda} | sed "s|/\(\S*\)\.gcda$|/\1.${id}.gcda|")
+    gcno=$(echo ${gcda} | sed 's|\.gcda|.gcno|')
+    new_gcno=$(echo ${gcda} | sed "s|\.gcda|.${id}.gcno|")
+    cp /tmp/gcov/${id}/${gcda} ${new_gcda}
+    cp ${gcno} ${new_gcno}
+    echo "  - ${new_gcda}"
+  done
+
+  cd src/mcl_3dl/
+  cp -r /catkin_ws/build ./
+
+  gcov $(find . -name "*.gcda") -p -c -l > /dev/null
+
+  rm -rf build/mcl_3dl_msgs
+  download_codecov='wget --timeout=10 -O /tmp/codecov https://codecov.io/bash'
+  ${download_codecov} || ${download_codecov} || ${download_codecov}
+  bash /tmp/codecov \
+    -Z \
+    -X gcov
+fi
 
 gh-pr-comment "${BUILD_LINK} PASSED on ${ROS_DISTRO}" "<details><summary>All tests passed</summary>
 
